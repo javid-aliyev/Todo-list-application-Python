ac595d83a6d7e5970d26dc578906cdac7ccb6fbd
Javid Aliyev <lorderer12@gmail.com>
2020-10-06 13:03:04 +0400

    unnecessary comments removed

--- db_creator.py
+++ db_creator.py
@@ -56,17 +56,3 @@ if __name__ == "__main__":
 			info("(=) Abort")
 	else:
 		error("wrong password!")
-
-# if __name__ == "__main__":
-# 	db_pass = secured_sinput("db password? ")
-# 	if hashlib.sha256(db_pass.encode("utf8")).hexdigest() == config.DB_PASS:
-# 		warn("!!that process will overwrite the database!!")
-# 		print(f"Do you actually want to create or overwrite the database({config.DB_PATH})? [y/n]")
-# 		npt = sinput("")
-# 		if len(npt) > 0 and npt[0].lower() != "n":
-# 			create_db()
-# 			success("(+) The database was created or overwritten")
-# 		else:
-# 			info("(=) Abort")
-# 	else:
-# 		error("wrong password!")
